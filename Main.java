8ac04cd949b2f2eb2032c296a83709f3305cadc4
Cheea9337 <cheea9337@wrdsb.ca>
2020-09-16 12:41:39 +0000

    finished biggest number problem

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * To find which number is bigger out of two numbers
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // get the user to enter a number
+    System.out.println("Please enter an integer:");
+    int a = input.nextInt();
+
+    // get the user to enter a second number
+    System.out.println("Please enter another integer:");
+    int b = input.nextInt();
+
+    // find out which number is bigger
+    if (a > b) {
+      System.out.println("The biggest number is " + a);
+    } else if (a < b) {
+      System.out.println("The biggest number is " + b);
+    } else {
+      System.out.println("There is no biggest number");
+    }
+
+
   }
 }
